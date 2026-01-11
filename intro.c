a1b0270786b070b85db554025f22252c4337ff1e
Piyu <piyamohanty18@gmail.com>
2026-01-11 23:19:22 +0530

    Update 1 on calc

--- intro.c
+++ intro.c
@@ -6,6 +6,7 @@ void main()
     printf("I am Piya [yes, just like the one mentioned in every bollywood song and yes people do sing aloud 'Oh re Piya' when I enter or 'Piyu bole' whenever I say something and also yes, I'm used to it { another useless bracket inside bracket just to annoy you ( LoL )}] and I'm a first year student at Silicon University, Bhubaneswar (never ask how I ended up here).\n\n");
     printf("I am pursuing my Bachelor Of Technology degree in Computer Science and Engineering and hence here I am, messing with repos and contributions, rawdogging my way through it in the hopes of being some sort of top dom in this shit by the time I push my first repo ( cuz there's no fees for spewing bullshit ).\n\n");
     printf("The next file after this will be a simple dumbass calculator program like they teach in schools and I have added it because why tf not. Thank you for reading till here and I hope you enjoy seeing how the code is written to do 2+2.!!\n\n");
-        printf("Yours menacingly,\n");
-        printf("Piya~\n");
+    printf("Yours menacingly,\n");
+    printf("Piya~\n\n");
+    printf("P.S.- tried to implement whatever was taught in sem 1 into the calculator and thought why not try out dynamic memory allocation since I never did any programs using that, so here we are. Will add history viewing function later because I fell asleep and fked up the schedule a bit :)\n");
 }
